cdd50afc6d54b939ec0267e046c26421c3cc3b68
alpmis <ajoldasov013@gmail.com>
2024-10-18 02:59:00 +0500

    switch-case

---
+++ index.js
@@ -0,0 +1,539 @@
+// Case 1
+// let kun = +prompt("Hafta kunini kiriting:")
+// console.log(kun + " - hafta kuni nomi : ");
+// switch (kun) {
+//     case 1:
+//         console.log(" Dushanba");
+//         break;
+//     case 2:
+//         console.log(" Seshanba");
+//         break
+//     case 3:
+//         console.log(" Chorshanba");
+//         break
+//     case 4:
+//         console.log(" Payshanba");
+//         break
+//     case 5:
+//         console.log(" Juma");
+//         break
+//     case 6:
+//         console.log(" Shanba");
+//         break
+//     case 7:
+//         console.log(" Yakshanba");
+//         break
+//     default:
+//         console.log(" Bunday hafta kuni yo'q!");
+
+//         break;
+// }
+
+// Case 2
+// let k=+prompt("Bahoni kiriting: ") 
+// console.log("Baho: " + k);
+// switch (k) {
+//     case 1:
+//         console.log("Yomon");
+//     break;
+//     case 2:
+//         console.log("qoniqarsiz");
+//     break;
+//     case 3:
+//         console.log("qoniqarli");
+//     break;
+//     case 4:
+//         console.log("Yahshi");
+//     break;
+//     case 5:
+//         console.log("A'lo");
+//     break;
+//     default:
+//         console.log("Bunday baho yo'q! Xato");
+//     break;
+// }
+
+// Case 3
+// let oy = +prompt("Oyni kiriting: ")
+// switch (oy) {
+//     case 1:
+//         console.log(oy + " - oy: Yanvar, Qish fasli!");
+//         break;
+//     case 2:
+//         console.log(oy + " - oy: Fevral, Qish fasli!");
+//         break;
+//     case 3:
+//         console.log(oy + " - oy: Mart, Bahor fasli!");
+//         break;
+//     case 4:
+//         console.log(oy + " - oy: Aprel, Bahor fasli!");
+//         break;
+//     case 5:
+//         console.log(oy + " - oy: May, Bahor fasli!");
+//         break;
+//     case 6:
+//         console.log(oy + " - oy: Iyun, Yoz fasli!");
+//         break;
+//     case 7:
+//         console.log(oy + " - oy: Iyul, Yoz fasli!");
+//         break;
+//     case 8:
+//         console.log(oy + " - oy: Avgust, Yoz fasli!");
+//         break;
+//     case 9:
+//         console.log(oy + " - oy: Sentyabr, Kuz fasli!");
+//         break;
+//     case 10:
+//         console.log(oy + " - oy: Oktabr, Kuz fasli!");
+//         break;
+//     case 11:
+//         console.log(oy + " - oy: Noyabr, Kuz fasli!");
+//         break;
+//     case 12:
+//         console.log(oy + " - oy: Dekabr, Qish fasli!");
+//         break;
+//     default:
+//     console.log("Bunaqa oy yo'q!");
+//         break;
+// }
+
+// If and Else If 3
+// let oy = +prompt("Oyni kiriting: ")
+// if(oy==1 || oy==2 || oy==12){
+//     console.log(oy+" - oy, Qish fasli");
+// }
+// else if(oy==3 ||oy == 4 || oy == 5 ){
+//     console.log(oy+" - oy, Bahor fasli");
+// }
+// else if(oy==6 ||oy == 7 || oy == 8 ){
+//     console.log(oy+" - oy, Yoz fasli");
+// }
+// else if(oy==9 ||oy == 10 || oy == 11 ){
+//     console.log(oy+" - oy, Kuz fasli");
+// }
+// else{
+//     console.log("Bunaqa oy yo'q!");
+// }
+
+// Case 4
+// let oy = +prompt("Oyni kiriting: ")
+// switch (oy) {
+//     case 1:
+//         console.log(oy + " - oy: Yanvar, 31 kun bor");
+//         break;
+//     case 2:
+//         console.log(oy + " - oy: Fevral, Kabisa bo'lmagan yili 28 kun, Kabisa yili 29 kun bor");
+//         break;
+//     case 3:
+//         console.log(oy + " - oy: Mart, 31 kun bor!");
+//         break;
+//     case 4:
+//         console.log(oy + " - oy: Aprel, 30 kun bor");
+//         break;
+//     case 5:
+//         console.log(oy + " - oy: May, 31 kun bor");
+//         break;
+//     case 6:
+//         console.log(oy + " - oy: Iyun, 30 kun bor");
+//         break;
+//     case 7:
+//         console.log(oy + " - oy: Iyul, 31 kun bor");
+//         break;
+//     case 8:
+//         console.log(oy + " - oy: Avgust, 31 kun bor");
+//         break;
+//     case 9:
+//         console.log(oy + " - oy: Sentyabr, 30 kun bor");
+//         break;
+//     case 10:
+//         console.log(oy + " - oy: Oktabr, 31 kun bor");
+//         break;
+//     case 11:
+//         console.log(oy + " - oy: Noyabr, 30 kun bor");
+//         break;
+//     case 12:
+//         console.log(oy + " - oy: Dekabr, 31 kun bor");
+//         break;
+//     default:
+//             console.log("Bunaqa oy yo'q!");
+//         break;
+// }
+
+// If and Else If
+// 
+
+// Case 5
+// let A = +prompt("A ga qiymat bering: ")
+// let B = +prompt("B ga qiymat bering: ")
+// let amal = +prompt("matematik amallar: (1 - '+'; 2 - '-'; 3 - '÷'; 4 - '*';)")
+// console.log("A = " + A + "; B = " + B + ";");
+// switch (amal) {
+//     case 1:
+//         console.log("A + B = " + (A + B));
+//         break;
+//     case 2:
+//         console.log("A - B = " + (A - B) + "; B - A = " + (B - A));
+//         break;
+//     case 3:
+//         console.log("A ÷ B = " + (A / B) + "; B ÷ A = " + (B / A));
+//         break;
+//     case 4:
+//         console.log("A x B = " + (A * B));
+//         break;
+//     default:
+//         console.log("Bunaqa amal yoq!");
+//         break;
+// }
+
+// Case 6
+// let kesma = +prompt("Kesma uzuligini kiriting: ")
+// let birlik = +prompt("O'lcham birligini kiriting: 1-dm; 2-km; 3-m; 4-mm; 5-sm;")
+// switch (birlik) {
+//     case 1:
+//         console.log("Kesma = " + kesma + " " + "desimetr --> " + (kesma / 10) + " metr");
+//         break;
+//     case 2:
+//         console.log("Kesma = " + kesma + " " + "kilometr --> " + (kesma * 10) + " metr");
+//         break;
+//     case 3:
+//         console.log("Kesma = " + kesma + " " + "metr");
+//         break;
+//     case 4:
+//         console.log("Kesma = " + kesma + " " + "millimetr --> " + (kesma / 1000) + " metr");
+//         break;
+//     case 5:
+//         console.log("Kesma = " + kesma + " " + "santimetr --> " + (kesma / 100) + " metr");
+//         break;
+//     default:
+//         console.log("Bunaqa o'lcham birlik yoq!");
+//         break;
+// }
+
+// Case 7
+// let yuk = +prompt("Kesma uzuligini kiriting: ")
+// let birlik = +prompt("O'lcham birligini kiriting: 1-kg; 2-mg; 3-g; 4-t; 5-ts;")
+// switch (birlik) {
+//     case 1:
+//         console.log("Yuk = " + yuk + " " + "kilogramm");
+//         break;
+//     case 2:
+//         console.log("Yuk = " + yuk + " " + "milligramm --> " + (yuk / 1000000) + " kilogramm");
+//         break;
+//     case 3:
+//         console.log("Yuk = " + yuk + " " + "gramm -->" +(yuk / 1000)+"kilogramm");
+//         break;
+//     case 4:
+//         console.log("Yuk = " + yuk + " " + "tonna --> " + (yuk * 1000) + " kilogramm");
+//         break;
+//     case 5:
+//         console.log("Yuk = " + yuk + " " + "tsentner --> " + (yuk * 100) + " kilogramm");
+//         break;
+//     default:
+//         console.log("Bunaqa o'lcham birlik yoq!");
+//         break;
+// }
+
+// Case 8
+// let D = +prompt("Kunni kiritinh: ")
+// let M = +prompt("Oyni kiritinh: ")
+// switch (M) {
+//     case 1:
+//         console.log(D + " - Yanvar");
+//         break;
+//     case 2:
+//         console.log(D + " - Fevral, Kabisa bo'lmagan yili 28 kun, Kabisa yili 29 kun bor");
+//         break;
+//     case 3:
+//         console.log(D + " - Mart");
+//         break;
+//     case 4:
+//         console.log(D+ " - Aprel");
+//         break;  
+//     case 5:
+//         console.log(D + " - May");
+//         break;
+//     case 6:
+//         console.log(D + " - Iyun");
+//         break;
+//     case 7:
+//         console.log(D + " - Iyul");
+//         break;
+//     case 8:
+//         console.log(D + " - Avgust");
+//         break;
+//     case 9:
+//         console.log(D + " - Sentyabr");
+//         break;
+//     case 10:
+//         console.log(D + " - Oktabr");
+//         break;
+//     case 11:
+//         console.log(D + " - Noyabr");
+//         break;
+//     case 12:
+//         console.log(D + " - Dekabr");
+//         break;
+//     default:
+//             console.log("Bunaqa oy yo'q!");
+//         break;
+// }
+
+// Case 9
+// let D = +prompt("Kunni kiritinh: ")
+// let M = +prompt("Oyni kiritinh: ")
+// switch (M) {
+//     case 1:
+//         console.log((D+=1) + " - Yanvar");
+//         break;
+//     case 2:
+//         console.log((D+=1) + " - Fevral, Kabisa bo'lmagan yili 28 kun, Kabisa yili 29 kun bor");
+//         break;
+//     case 3:
+//         console.log((D+=1)  + " - Mart");
+//         break;
+//     case 4:
+//         console.log((D+=1) + " - Aprel");
+//         break;  
+//     case 5:
+//         console.log((D+=1) + " - May");
+//         break;
+//     case 6:
+//         console.log((D+=1) + " - Iyun");
+//         break;
+//     case 7:
+//         console.log((D+=1) + " - Iyul");
+//         break;
+//     case 8:
+//         console.log((D+=1) + " - Avgust");
+//         break;
+//     case 9:
+//         console.log((D+=1) + " - Sentyabr");
+//         break;
+//     case 10:
+//         console.log((D+=1) + " - Oktabr");
+//         break;
+//     case 11:
+//         console.log((D+=1) + " - Noyabr");
+//         break;
+//     case 12:
+//         console.log((D+=1) + " - Dekabr");
+//         break;
+//     default:
+//             console.log("Bunaqa oy yo'q!");
+//         break;
+// }
+
+// Case 10
+// let Y = prompt("Robot yo'nalishini kiriting: 's'-shimol; 'j'-janub; 'q'-sharq; 'g'-g'arb;")
+// let K = +prompt("Robotga komanda: 0-harakatni davom ettir; 1-chapga buril; 2-o'ngga buril")
+// switch (Y) {
+//     case 's':
+//         console.log("Robot shimolda ketyapti, ");
+//         break;
+//     case 'j':
+//         console.log("Robot janubda ketyapti, ");
+//         break
+//     case 'q':
+//         console.log("Robot sharqda ketyapti, ");
+//         break
+//     case 'g':
+//         console.log("Robot g'arbda ketyapti, ");
+//         break
+//     default:
+//         console.log("Yo'nalish topilmadi!");
+//         break;
+// }
+// switch (K) {
+//     case 0:
+//         console.log("Robotga 'harakatni davom ettir' komandasi berildi");
+//         break;
+//     case 1:
+//         console.log("Robotga 'chapga buril' komandasi berildi");
+//         break
+//     case 2:
+//         console.log("Robotga 'o'nnga buril' komandasi berildi");
+//         break
+//     default:
+//         console.log("Unaqa komanda yoq!");
+//         break;
+// }
+
+// Case 11
+// let Y = prompt("Lokatr yo'nalishini kiriting: 's'-shimol; 'j'-janub; 'q'-sharq; 'g'-g'arb;")
+// let K1 = +prompt("Lokatrga komanda: 0-o'nga buril; 1-chapga buril; 2-burilish 180 gradus")
+// let K2 = +prompt("Lokatrga komanda: 0-o'nga buril; 1-chapga buril; 2-burilish 180 gradus")
+
+// switch (Y) {
+//     case 's':
+//         console.log("Lokatr shimolda ketyapti, ");
+//         break;
+//     case 'j':
+//         console.log("Lokatr janubda ketyapti, ");
+//         break
+//     case 'q':
+//         console.log("Lokatr sharqda ketyapti, ");
+//         break
+//     case 'g':
+//         console.log("Lokatr g'arbda ketyapti, ");
+//         break
+//     default:
+//         console.log("Yo'nalish topilmadi!");
+//         break;
+// }
+// switch (K1) {
+//     case 0:
+//         console.log("Lokatrga 'O'nnga buril' komandasi berildi");
+//         break;
+//     case 1:
+//         console.log("Lokatrga 'Chapga buril' komandasi berildi");
+//         break
+//     case 2:
+//         console.log("Lokatrga 'burilish 180 gradus' komandasi berildi");
+//         break
+//     default:
+//         console.log("Unaqa komanda yoq!");
+//         break;
+// }
+// switch (K2) {
+//     case 0:
+//         console.log("keyin 'O'nnga buril' komandasi berildi");
+//         break;
+//     case 1:
+//         console.log("keyin 'Chapga buril' komandasi berildi");
+//         break
+//     case 2:
+//         console.log("keyin 'burilish 180 gradus' komandasi berildi");
+//         break
+//     default:
+//         console.log("Unaqa komanda yoq!");
+//         break;
+// }
+
+// Case 12
+// let d = +prompt("Doira elementini kiriting: ")
+// let M = +prompt("Doiraning umumiy o'lchamlari: ")
+// let p = Math.PI
+// switch (M) {
+//     case 1:
+//         console.log("Doiraning radiusi berilgan: R = " + d + " m;  Qolganlari: D = " + (2 * d) + " m;");
+//         console.log("L = " + (2 * p * d) + " m; S = " + (p * d * d)+" m^2");
+//         break;
+//     case 2:
+//         console.log("Doiraning diametri berilgan: D = " + d + "m; Qolganlari: R = " + (d/2) + " m;");
+//         console.log("L = " + (p * d) + " m; S = " + (p * d * d/4)+" m^2");
+//         break;
+//     case 3:
+//         console.log("Doiraning uzunligi berilgan: L = " + d + "m; Qolganlari: R = " + d/(2*p) + " m;");
+//         console.log("D = " + (d/p) + " m; S = " + ((d*d)/(4*p))+" m^2");
+//         break;
+//     case 4:
+//         console.log("Doiraning maydoni berilgan: S = " + d+" m^2; Qolganlari: R = " + Math.sqrt(d/p) + " m;");
+//         console.log("D = " + (2 * Math.sqrt(d/p)) + " m; L = " + (2 * p * Math.sqrt(d/p)) + " m;");
+//         break;
+//     default:
+//         console.log("Unaqa o'lcham yoq!");
+//         break;
+// }
+
+// Case 13
+// let d = +prompt("Teng yonli uchburchakning elementini kiriting: ")
+// let M = +prompt("Teng yonli uchburchakning umumiy o'lchamlari: ")
+// switch (M) {
+//     case 1:
+//         console.log("Teng yonli uchburchakning a kateti berilgan: a = " + d + " m;  Qolganlari: c = " + (d*Math.sqrt(2)) + " m;");
+//         console.log("h = " + ((d*Math.sqrt(2))/2) + " m; S = " + (d/(2*Math.sqrt(2)))+" m^2");
+//         break;
+//     case 2:
+//         console.log("Teng yonli uchburchakning c gipotenuzasi berilgan: c = " + d + "m; Qolganlari: a = " + (d/Math.sqrt(2)) + " m;");
+//         console.log("h = " + (d/2) + " m; S = " + (d * d/4)+" m^2");
+//         break;
+//     case 3:
+//         console.log("Teng yonli uchburchakning balandligi berilgan: h = " + d + "m; Qolganlari: a = " + (d*Math.sqrt(2)) + " m;");
+//         console.log("c = " + (2*d) + " m; S = " + (d*d)+" m^2");
+//         break;
+//     case 4:
+//         console.log("Teng yonli uchburchakning maydoni berilgan: S = " + d+" m^2; Qolganlari: a = " + Math.sqrt(2*d) + " m;");
+//         console.log("c = " + (2 * Math.sqrt(d)) + " m; h = " + Math.sqrt(d) + " m;");
+//         break;
+//     default:
+//         console.log("Unaqa o'lcham yoq!");
+//         break;
+// }
+
+// Case 14
+// let d = +prompt("Teng tomonli uchburchakning elementini kiriting: ")
+// let M = +prompt("Teng tomonli uchburchakning umumiy o'lchamlari: ")
+// switch (M) {
+//     case 1:
+//         console.log("Teng tomonli uchburchakning a tomoni berilgan: a = " + d + " m;  Qolganlari: R1 = " + (d*Math.sqrt(3)/6) + " m;");
+//         console.log("R2 = " + (d/Math.sqrt(3)) + " m; S = " + (d*d*Math.sqrt(3)/4)+" m^2");
+//         break;
+//     case 2:
+//         console.log("Teng tomonli uchburchakga ichki chizilgan aylananing radiusi berilgan: R1 = " + d + "m; Qolganlari: a = " + (d*2*Math.sqrt(3)) + " m;");
+//         console.log("R2 = " + (4*d*Math.sqrt(3)) + " m; S = " + (3*Math.sqrt(3)*d*d)+" m^2");
+//         break;
+//     case 3:
+//         console.log("Teng tomonli uchburchakga tashqi chizilgan aylananing radiusi berilgan: R2 = " + d + "m; Qolganlari: a = " + (d*Math.sqrt(3)) + " m;");
+//         console.log("R1 = " + (d/2) + " m; S = " + (d*d*3*Math.sqrt(3)/4)+" m^2");
+//         break;
+//     case 4:
+//         console.log("Teng tomonli uchburchakning maydoni berilgan: S = " + d+" m^2; Qolganlari: a = " + 2*Math.sqrt(d)/Math.pow(3 , 4) + " m;");
+//         console.log("R1 = " + (Math.sqrt(d)/Math.pow(3 , 4))/Math.sqrt(3) + " m; R2 = " + (2*Math.sqrt(d)/Math.pow(3 , 4))/Math.sqrt(3) + " m;");
+//         break;
+//     default:
+//         console.log("Unaqa o'lcham yoq!");
+//         break;
+// }
+
+// Case 15
+// let N = +prompt("Karta qiymatini liriting: (6 <= N <= 14)")
+// let M = +prompt("Karta turini kiriting: 1-g'isht; 2-olma; 3-chillak; 4-qarg'a;")
+// switch (N) {
+//     case 6:
+//         console.log("oltti");
+//         break;
+//     case 7:
+//         console.log("yetti");
+//         break;
+//     case 8:
+//         console.log("sakkiz");
+//         break;
+//     case 9:
+//         console.log("toqqiz");
+//         break;
+//     case 10:
+//         console.log("on");
+//         break;
+//     case 11:
+//         console.log("valet");
+//         break;
+//     case 12:
+//         console.log("dama");
+//         break;
+//     case 13:
+//         console.log("qirol");
+//         break;
+//     case 14:
+//         console.log("tuz");
+//         break;
+//     default:
+//         console.log("Unaqa karta qiymati yoq!");
+//         break;
+// }
+// switch (M) {
+//     case 1:
+//         console.log("g'isht");
+//         break;
+//     case 2:
+//         console.log("olma");
+//         break;
+//     case 3:
+//         console.log("chillak");
+//         break;
+//     case 4:
+//         console.log("qarg'a");
+//         break;
+//     default:
+//         console.log("Unaqa karta turi yo'q");
+//         break;
+// }
